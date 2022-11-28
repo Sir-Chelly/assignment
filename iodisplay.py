06af37fc25504b896a5b134dc7c18577ddd77b64
Sir-Chelly <118954710+Sir-Chelly@users.noreply.github.com>
2022-11-28 11:53:13 +0100

    Create iodisplay.py

---
+++ iodisplay.py
@@ -0,0 +1,15 @@
+# Name: Idima Ikenna Uzochi
+# Reg: 2017364004
+# Dept: ECE 
+# Course: ECE 541
+# Lecturer: Engr Dozie
+
+# Question: Create a Project that will prompt users to enter their name,age and department and display the out put
+
+name = input('Enter your name: ')
+age = input('Enter your age: ')
+department = input('Enter your department: ')
+
+print('Your name is ' + name)
+print('You are ' + age + ' years old')
+print('And are in the department of ' + department)
